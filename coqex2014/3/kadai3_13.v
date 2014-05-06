8896d71c7a510ae0abee6eb666a6a5953ebfa73e
odan <odan3240@gmail.com>
2014-05-06 23:36:53 +0900

    Add kadai3_13.v

---
+++ coqex2014/3/kadai3_13.v
@@ -0,0 +1,24 @@
+(* コンストラクタの名前は例えば、SOとSにするとよい。 *)
+Inductive pos : Set :=
+  | SO : pos
+  | S  : pos -> pos
+  (* posの定義を書く *) .
+
+Fixpoint plus(n m:pos) : pos := 
+  match n with
+  | SO => m
+  | S p => S (plus p m)
+  end.
+
+Infix "+" := plus.
+
+Theorem plus_assoc : forall n m p, n + (m + p) = (n + m) + p.
+Proof.
+  intros.
+  induction n.
+  simpl.
+  reflexivity.
+  simpl.
+  rewrite IHn.
+  reflexivity.
+Qed.
